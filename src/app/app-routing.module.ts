54ae3e90e2de4919a51d41ebe85ba9101a2ab268
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2024-02-28 12:24:48 -0500

    FIREBASE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {
-    path: '',
+    path: '/',
     redirectTo: 'inicio',
     pathMatch: 'full'
   },
